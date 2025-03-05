31f6eb2654872a6f04d6092e5c88e9c340808220
Nataliya <yavtushenkon@gmail.com>
2025-03-05 19:37:29 +0200

    delete

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,14 +1,41 @@
 import ContactCollection from '../models/contact.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactCollection.find();
-};
+export function getAllContacts() {
+  return ContactCollection.find();
+}
 
-export const getContactById = async (contactid) => {
-  const contact = await ContactCollection.findById(contactid);
-};
+export function getContactById(contactid) {
+  return ContactCollection.findById(contactid);
+}
 
 export const createContact = async (payload) => {
   const contact = await ContactCollection.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactid, payload, options = {}) => {
+  const rawResult = await ContactCollection.findOneAndUpdate(
+    { _id: contactid },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactid) => {
+  const contact = await ContactCollection.findOneAndDelete({
+    _id: contactid,
+  });
+
   return contact;
 };
