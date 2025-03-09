b126744705f793871e5ccea0a8e3eff647a4e3be
Nataliya <yavtushenkon@gmail.com>
2025-03-09 19:40:33 +0200

    sort

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,13 @@
+import { SORT_ORDER } from '../constants/index.js';
 import ContactCollection from '../models/contact.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async ({ page, perPage }) => {
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
