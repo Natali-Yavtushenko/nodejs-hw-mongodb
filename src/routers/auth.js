fa2eef992990912d8a56075d5f678ccf1f5cacf2
Nataliya <yavtushenkon@gmail.com>
2025-03-14 13:35:47 +0200

    user

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { validateBody } from '../middlewares/validateBody';
-import { registerUserSchema } from '../validation/auth';
-import { ctrlWrapper } from '../utils/ctrlWrapper';
-import { registerUserController } from '../controllers/auth';
+import { validateBody } from '../middlewares/validateBody.js';
+import { registerUserSchema } from '../validation/auth.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { registerUserController } from '../controllers/auth.js';
+
+const router = Router();
 
 router.post(
   '/register',
