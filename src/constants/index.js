12fa915b39351896c58a840178cf61837e64c237
Nataliya <yavtushenkon@gmail.com>
2025-03-24 23:39:47 +0200

    password

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import patch from 'node:path';
+
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
@@ -6,3 +8,13 @@ export const SORT_ORDER = {
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 export const THIRTY_DAYS = 30 * ONE_DAY;
+
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
+
+export const TEMPLATES_DIR = patch.join(process.cwd(), 'src', 'templates');
