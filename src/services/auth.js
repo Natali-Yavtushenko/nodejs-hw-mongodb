68e282f7f57acae9e9826f93d2b55425ff9115fc
Nataliya <yavtushenkon@gmail.com>
2025-04-10 11:42:22 +0300

    email

--- src/services/auth.js
+++ src/services/auth.js
@@ -113,7 +113,7 @@ export const requestResetToken = async (email) => {
 
   const resetPasswordTemplatePath = path.join(
     TEMPLATES_DIR,
-    'reset-password-email.html',
+    'reset-pwd-email.html',
   );
 
   try {
