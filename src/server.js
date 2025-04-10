b99b148272612623ee6ffd2f7d26057e0a1544f7
Nataliya <yavtushenkon@gmail.com>
2025-04-10 10:26:58 +0300

    env 6

--- src/server.js
+++ src/server.js
@@ -15,8 +15,11 @@ export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
+
   app.use(cors());
+
   app.use(cookieParser());
+
   app.use(
     pino({
       transport: {
